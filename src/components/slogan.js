e3d3324984f36133dbf47f6438463825cd2de5d1
Robin Wijnant <robin@wijnant.me>
2019-08-25 11:46:08 +0200

    min slogan height

--- src/components/slogan.js
+++ src/components/slogan.js
@@ -10,12 +10,12 @@ const H2 = styled.h2`
   font-weight: 700;
   line-height: 150%;
   letter-spacing: 0.04em;
-  height: 75px;
+  min-height: 75px;
 
   @media (max-width: 600px) {
     font-size: 20px;
     margin: 60px 0;
-    height: 55px;
+    min-height: 55px;
   }
 `
 const cursorStyles = css`
