dc8ca27091591e08182797c0c542dfa1b87c5660
Robin Wijnant <robin@wijnant.me>
2019-08-27 15:33:09 +0200

    css caniuse test

--- src/components/project-picker.js
+++ src/components/project-picker.js
@@ -3,12 +3,13 @@ import styled from "@emotion/styled"
 import styleVars from "../styles/vars"
 import Wrapper from "./wrapper"
 
-const Container = styled(Wrapper)`
+const Arrow = styled.button`
+  all: unset;
 `
 
 const ProjectPicker = ({className}) => (
   <Wrapper className={className}>
-    
+    <Arrow>This is an unstyled button</Arrow>
   </Wrapper>
 )
 
--- src/components/wrapper.js
+++ src/components/wrapper.js
@@ -5,12 +5,13 @@ import styleVars from "../styles/vars"
 const Container = styled.div`
   max-width: ${styleVars.spacing.maxWidth};
   padding: 0 ${styleVars.spacing.sideMargin};
+  margin: 0 auto;
   box-sizing: border-box;
 `
 
-const Wrapper = ({className}) => (
+const Wrapper = ({className, children}) => (
   <Container className={className}>
-  
+    {children}
   </Container>
 )
 
