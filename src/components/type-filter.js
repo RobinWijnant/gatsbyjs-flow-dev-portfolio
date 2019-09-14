b34b80bfa9a47bb8357e0e92a8db9c1536301eb9
Robin Wijnant <robin@wijnant.me>
2019-09-14 19:30:25 +0200

    responsive type filter

--- src/components/type-filter.js
+++ src/components/type-filter.js
@@ -4,16 +4,21 @@ import styled from "@emotion/styled"
 import ClickableProjectType from "./clickable-project-type"
 import filterIcon from "../images/icons/filter.svg"
 
-const List = styled.ul`
+const Container = styled.div`
   display: flex;
   justify-content: flex-end;
+  align-items: flex-start;
+`
+const List = styled.ul`
+  display: inline-flex;
+  justify-content: flex-start;
   flex-wrap: wrap;
-  margin-right: -10px;
+  margin: 0 -10px 0 0;
   padding: 0;
 `
 const FilterIcon = styled.img`
   width: 20px;
-  margin: -8px 10px 0 0;
+  margin: 8px 10px 0 0;
 `
 const ListItem = styled.li`
   list-style-type: none;
@@ -57,22 +62,24 @@ class TypeFilter extends React.Component<Props, State> {
 
   render() {
     return (
-      <List className={this.props.className}>
+      <Container className={this.props.className}>
         <FilterIcon src={filterIcon} alt={"Filter icon"} />
-        {this.types.map((type: string, index: number) => {
-          return (
-            <ListItem key={index}>
-              <ClickableProjectType
-                onClick={this.toggleFilter.bind(this, type)}
-                active={this.state.selectedTypes.includes(type)}
-                size={1.2}
-              >
-                {type}
-              </ClickableProjectType>
-            </ListItem>
-          )
-        })}
-      </List>
+        <List>
+          {this.types.map((type: string, index: number) => {
+            return (
+              <ListItem key={index}>
+                <ClickableProjectType
+                  onClick={this.toggleFilter.bind(this, type)}
+                  active={this.state.selectedTypes.includes(type)}
+                  size={1.2}
+                >
+                  {type}
+                </ClickableProjectType>
+              </ListItem>
+            )
+          })}
+        </List>
+      </Container>
     )
   }
 }
